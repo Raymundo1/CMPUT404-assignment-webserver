149c9c0e3a436714a91f87ef06297729ed23f26e
Raymundo1 <cquptcxl@gmail.com>
2019-01-25 10:02:23 -0700

    add copyright

--- server.py
+++ server.py
@@ -5,6 +5,7 @@ import html
 import os
 
 # Copyright 2013 Abram Hindle, Eddie Antonio Santos
+# Modified work Copyright 2019 Xinlei Chen
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
